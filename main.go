ad722c655ec7d7fdc0aa5f3b36709697a131610b
Tahsin Rahman <tr174@duke.edu>
2025-01-17 20:54:27 -0500

    Finished code, need to debug

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
+	"log"
+	"fmt"
+	"os"
 	"github.com/gocolly/colly"
 )
 
@@ -35,13 +39,43 @@ func main(){
 		stock := Stock{}
 		stock.company = e.ChildText('h1')
 		fmt.Println("Company:", stock.company)
-		stock.price = e.ChildTExt("fin-streamer[data-field='regularMarketPrice]")
+		stock.price = e.ChildText("fin-streamer[data-field='regularMarketPrice]")
 		fmt.Println("Price:", stock.price)
 		stock.change = e.ChildText("fin-streamer[data-field='regularMarketChangePercent']")
 		fmt.Println("Change:", stock.change)
 
 		stocks = append(stocks, stock)
 	})
-	c.Visit("https://finance.yahoo.com/quote/" + t + "/")
+
+	c.Wait()
+	for _, t:= range ticker {
+		c.Visit("https://finance.yahoo.com/quote/" + t + "/")
+	}
+	fmt.Println (stocks)
+	file, err := os.Create(stocks.csv)
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+
+	}
+
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	headers := []string{
+		"company",
+		"price",
+		"change",
+	}
+
+	writer.Write(headers)
+	for _, stock := range stocks {
+		record := []string{
+			stock.company,
+			stock.price,
+			stock.change,
+		}
+
+	writer.Write(record)
+	}
+	defer writer.Flush()
 
 }
